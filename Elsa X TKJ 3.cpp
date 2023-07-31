2bd5ff95f72b2244d7fe48ba11a36ec2f1959e3b
bismillahbg <140993214+bismillahbg@users.noreply.github.com>
2023-07-31 09:31:53 +0700

    Add files via upload

---
+++ Elsa X TKJ 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+char name[15], hobi[20], umur[15];
+
+int main(){
+    count<<"\t\t====PROGRAM BIODATA SEDERHANA====\t\t"<<endl;
+    cout<<"Siapa nama Anda?"<<endl;
+    cin>>nama;Elsa Syarif
+    cout<<"Apa hobi Anda?"<<endl;
+    cin>>hobi;membaca novel
+    count<<"Berapa umur Anda?"<<endl;
+    cin>>umur;16
+    count<<"\n\tHalo saya:<<nama<<endl;
+    count<<"\tHobi saya:<<hobi<<endl;
+    count<<"\tUmur saya:<<umur<<endl;
+
+return 0;
+
+
